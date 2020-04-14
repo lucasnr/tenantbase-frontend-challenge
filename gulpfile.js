9e779f54efca08a03723f35fdc43e351e6fd0ef6
Lucas do Nascimento <lucasnascimentoribeiro13@gmail.com>
2020-04-14 16:51:09 -0300

    Add gulp css build plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
-const { src, dest, series, parallel } = require('gulp'),
+const { src, dest, series } = require('gulp'),
 	clean = require('gulp-clean'),
+	inlineSource = require('gulp-inline-source'),
+	htmlReplace = require('gulp-html-replace'),
+	concatCss = require('gulp-concat-css'),
+	cleanCss = require('gulp-clean-css'),
 	htmlmin = require('gulp-htmlmin');
 
 function cleanDist() {
@@ -10,10 +14,38 @@ function copyDist() {
 	return src('src/**/*').pipe(dest('dist/'));
 }
 
+function inlineJs() {
+	return src('src/**/*.html').pipe(inlineSource()).pipe(dest('dist/'));
+}
+
+function minifyCss() {
+	return src('src/assets/css/**/*.css')
+		.pipe(concatCss('styles.min.css'))
+		.pipe(cleanCss())
+		.pipe(dest('dist/assets/css'));
+}
+
+function buildHtml() {
+	return src('dist/**/*.html')
+		.pipe(
+			htmlReplace({
+				css: 'assets/css/styles.min.css',
+			})
+		)
+		.pipe(dest('dist/'));
+}
+
 function minifyHtml() {
 	return src('dist/**/*.html')
 		.pipe(htmlmin({ collapseWhitespace: true }))
 		.pipe(dest('dist/'));
 }
 
-exports.default = series(cleanDist, copyDist, parallel(minifyHtml));
+exports.default = series(
+	cleanDist,
+	copyDist,
+	inlineJs,
+	minifyCss,
+	buildHtml,
+	minifyHtml
+);
